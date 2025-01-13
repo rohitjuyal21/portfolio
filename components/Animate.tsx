05645a25a3b09489c94996fde4830d1caa43b37a
rohitjuyal21 <rohitjuyal2003@gmail.com>
2025-01-13 13:26:10 +0530

    update

--- components/Animate.tsx
+++ components/Animate.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 interface AnimateProps {
   children: React.ReactNode;
@@ -8,18 +8,16 @@ interface AnimateProps {
 
 export default function Animate({ children, delay = 0 }: AnimateProps) {
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ height: 0, opacity: 0, y: 10, filter: "blur(10px)" }}
-        animate={{ height: "100%", opacity: 1, y: 0, filter: "blur(0px)" }}
-        exit={{
-          height: 0,
-          opacity: 0,
-        }}
-        transition={{ duration: 0.5, delay }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      initial={{ height: 0, opacity: 0, y: 10, filter: "blur(10px)" }}
+      animate={{ height: "100%", opacity: 1, y: 0, filter: "blur(0px)" }}
+      exit={{
+        height: 0,
+        opacity: 0,
+      }}
+      transition={{ duration: 0.5, delay }}
+    >
+      {children}
+    </motion.div>
   );
 }
--- components/ui/card-border.tsx
+++ components/ui/card-border.tsx
@@ -11,7 +11,6 @@ export default function CardBorder({
       <span className="absolute top-0 left-0 h-full w-px bg-gradient-to-b from-sky-500/50 to-sky-500/0"></span>
       <span className="absolute bottom-0 h-px w-full bg-gradient-to-r  from-sky-500/0 to-sky-500/50 left-0"></span>
       <span className="absolute top-0 right-0 h-full w-px bg-gradient-to-b  from-sky-500/0 to-sky-500/50"></span>
-
       {children}
     </div>
   );
